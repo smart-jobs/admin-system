84d2ee33a17171db5bf4f1f648963f8c29c1b9c4
dygapp <dygapp@qq.com>
2018-11-27 17:03:27 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['plugin:vue/essential', '@vue/prettier'],
+  plugins: ['vue'],
+  rules: {
+    'max-len': [
+      'error',
+      {
+        code: 250,
+      },
+    ],
+    'no-unused-vars': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'es5',
+        bracketSpacing: true,
+        jsxBracketSameLine: true,
+      },
+    ],
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+};
